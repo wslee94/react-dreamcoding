908a26302f8a80f7300d0dd185e7d7bdc8a8cb8c
wslee94 <leemuo123@gmail.com>
2022-12-07 22:39:11 +0900

    inputText 구현

--- youtube/src/components/atoms/InputText/InputText.js
+++ youtube/src/components/atoms/InputText/InputText.js
@@ -1,5 +1,23 @@
 import React from "react";
+import styles from "./InputText.module.css";
 
-export default function InputText() {
-  return <div>InputText</div>;
+export default function InputText({
+  value,
+  onChange,
+  placeholder,
+  ...otherProps
+}) {
+  const className = otherProps.className
+    ? [styles.input, otherProps.className].join(" ")
+    : styles.input;
+
+  return (
+    <input
+      {...otherProps}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      className={className}
+    />
+  );
 }
--- youtube/src/components/molecules/SearchBar/SearchBar.js
+++ youtube/src/components/molecules/SearchBar/SearchBar.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SearchBar.module.css";
-import { Button } from "../../../components";
+import { Button, InputText } from "../../../components";
 import { AiOutlineSearch } from "react-icons/ai";
 
 export default function SearchBar() {
+  const [keyword, setKeyword] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
-    <form autoComplete="off" onSubmit={handleSubmit}>
+    <form className={styles.form} autoComplete="off" onSubmit={handleSubmit}>
+      <InputText
+        className={styles.input}
+        value={keyword}
+        placeholder="검색"
+        onChange={(e) => setKeyword(e.target.value)}
+      />
       <Button onClick={() => {}} className={styles.button}>
         <AiOutlineSearch className={styles.icon} />
       </Button>
