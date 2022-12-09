2a91680624105cf0ebcb2000e9cefa0d861b0eb4
wslee94 <leemuo123@gmail.com>
2022-12-09 23:25:55 +0900

    Card 작성

--- youtube/src/components/molecules/Card/Card.js
+++ youtube/src/components/molecules/Card/Card.js
@@ -2,15 +2,17 @@ import React from "react";
 import styles from "./Card.module.css";
 
 export default function Card({ thumbnailURL, title, author, date }) {
+  const timeAgo = new TimeAgo("en-US");
+
   return (
-    <div>
+    <div className={styles.card}>
       <div className={styles.thumbnail_wrapper}>
         <img className={styles.thumbnail} alt="thumbnail" src={thumbnailURL} />
       </div>
       <div>
         <p className={styles.title}>{title}</p>
         <p className={styles.author}>{author}</p>
-        <p className={styles.date}>{date}</p>
+        <p className={styles.date}>{timeAgo.format(date)}</p>
       </div>
     </div>
   );
