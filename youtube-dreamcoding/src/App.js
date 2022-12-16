3c9e008c6f2cd71af0ac9f292c933312c6ff1d8a
wslee94 <leemuo123@gmail.com>
2022-12-17 00:10:27 +0900

    youtube-dreamcoding SearchHeader 구현

--- youtube-dreamcoding/src/App.js
+++ youtube-dreamcoding/src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import { Outlet } from "react-router-dom";
+import SearchHeader from "./components/SearchHeader";
 
 function App() {
   return (
     <>
+      <SearchHeader />
       <Outlet />
     </>
   );
