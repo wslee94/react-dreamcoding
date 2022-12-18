43658a49b4b8128b7677b46ad7d4c961cbce5afa
wslee94 <leemuo123@gmail.com>
2022-12-18 19:01:15 +0900

    youtube 구현

--- youtube-dreamcoding/src/api/youtubeClient.js
+++ youtube-dreamcoding/src/api/youtubeClient.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export default class YoutubeClient {
   constructor() {
-    console.log(process.env.REACT_APP_YOUTUBE_API_KEY);
     this.httpClient = axios.create({
       baseURL: "https://www.googleapis.com/youtube/v3",
       params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
