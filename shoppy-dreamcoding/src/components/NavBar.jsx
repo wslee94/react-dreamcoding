0e6a9e71ec0af2fd6d5c5c9ed79b66c164858f04
wslee94 <leemuo123@gmail.com>
2022-12-29 14:20:02 +0900

    Shoppy Nav 구현 및 Admin 여부에 따른 UI 변화 적용

---
+++ shoppy-dreamcoding/src/components/NavBar.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FiShoppingBag } from "react-icons/fi";
+import { BsFillPencilFill } from "react-icons/bs";
+import { login, logout, onUserStateChange } from "../api/firebase";
+import User from "./User";
+import Button from "./ui/Button";
+
+export default function NavBar() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange(setUser);
+  }, []);
+
+  return (
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      <Link to="/" className="flex items-center text-4xl text-brand">
+        <FiShoppingBag />
+        <h1>Shoppy</h1>
+      </Link>
+      <nav className="flex items-center gap-4 font-semibold">
+        <Link to="/products">Products</Link>
+        <Link to="/carts">carts</Link>
+        {user?.isAdmin && (
+          <Link to="/products/new" className="text-2xl">
+            <BsFillPencilFill />
+          </Link>
+        )}
+        {user && <User user={user} />}
+        {!user && <Button onClick={login} text="Login" />}
+        {user && <Button onClick={logout} text="Logout" />}
+      </nav>
+    </header>
+  );
+}
