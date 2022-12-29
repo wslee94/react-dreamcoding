2f016d1d31b664b40411eb70b8a2e92235bd9249
wslee94 <leemuo123@gmail.com>
2022-12-29 17:23:01 +0900

    Shoppy 제품목록 구현

--- shoppy-dreamcoding/src/App.js
+++ shoppy-dreamcoding/src/App.js
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthContextProvider } from "./components/context/AuthContext";
 import NavBar from "./components/NavBar";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <AuthContextProvider>
-      <NavBar />
-      <Outlet />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <NavBar />
+        <Outlet />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
--- shoppy-dreamcoding/src/api/firebase.js
+++ shoppy-dreamcoding/src/api/firebase.js
@@ -54,12 +54,20 @@ async function adminUser(user) {
 
 export async function addNewProduct(product, image) {
   const id = uuid();
-  console.log(product, image);
   return set(ref(database, `products/${id}`), {
     ...product,
     id,
     price: parseInt(product.price),
     image,
     options: product.options.split(","),
+  });
+}
+
+export async function getProducts() {
+  return get(ref(database, "products")).then((snapshot) => {
+    if (snapshot.exists()) {
+      return Object.values(snapshot.val());
+    }
+    return [];
   });
 }
