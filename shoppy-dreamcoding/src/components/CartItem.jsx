85996889ada310a7b932268269ec861fb886867f
wslee94 <leemuo123@gmail.com>
2022-12-29 22:08:26 +0900

    Shoppy 리팩터링

--- shoppy-dreamcoding/src/components/CartItem.jsx
+++ shoppy-dreamcoding/src/components/CartItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AiOutlineMinusSquare, AiOutlinePlusSquare } from "react-icons/ai";
 import { RiDeleteBin5Fill } from "react-icons/ri";
-import { addOrUpdateToCart, removeFromCart } from "../api/firebase";
+import useCart from "../hooks/useCart";
 
 const ICON_CLASS =
   "transition-all cursor-pointer hover:text-brand hover:scale-105 mx-1";
@@ -9,17 +9,17 @@ const ICON_CLASS =
 export default function CartItem({
   product,
   product: { id, image, title, option, quantity, price },
-  uid,
 }) {
+  const { addOrUpdateItem, removeItem } = useCart();
   const handleMinus = () => {
     if (quantity < 2) return;
-    addOrUpdateToCart(uid, { ...product, quantity: quantity - 1 });
+    addOrUpdateItem.mutate({ ...product, quantity: quantity - 1 });
   };
   const handlePlus = () => {
-    addOrUpdateToCart(uid, { ...product, quantity: quantity + 1 });
+    addOrUpdateItem.mutate({ ...product, quantity: quantity + 1 });
   };
   const handleDelete = () => {
-    removeFromCart(uid, id);
+    removeItem.mutate(id);
   };
 
   return (
---
+++ shoppy-dreamcoding/src/hooks/useProducts.jsx
@@ -0,0 +1,17 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getProducts, addNewProduct } from "../api/firebase";
+
+export default function useProducts() {
+  const queryClient = useQueryClient();
+
+  const productQuery = useQuery(["products"], getProducts, {
+    staleTime: 1000 * 60,
+  });
+
+  const addProduct = useMutation(
+    ({ product, url }) => addNewProduct(product, url),
+    { onSuccess: () => queryClient.invalidateQueries(["products"]) }
+  );
+
+  return { productQuery, addProduct };
+}
